2d188731cf6ae0946299771df3e78d276830b7d2
Bastian Widmer <b.widmer@dasrecht.net>
2023-09-10 10:06:28 +0200

    adding more discovered versions and updating script slightly

--- 0_D0002_18/enumerate-versions.sh
+++ 0_D0002_18/enumerate-versions.sh
@@ -1,1 +1,3 @@
+for i in {0..99} ; do wget -nc http://47.254.36.66/0_D0002_18/MW3_16U_5406_1.$i.bin ; done
 for i in {0..99} ; do wget -nc http://47.254.36.66/0_D0002_18/MW3_16U_5406_2.$i.bin ; done
+for i in {0..99} ; do wget -nc http://47.254.36.66/0_D0002_18/MW3_15_0501_1.$i.bin ; done
